a08f918cba958a537a9f4c8b1fee08c86e6feedf
oycyqr <1456682842@qq.com>
2020-04-15 18:03:12 +0800

    Initial commit

---
+++ src/main/java/com/oycbest/jdbcdemo/controller/JdbcController.java
@@ -0,0 +1,49 @@
+package com.oycbest.jdbcdemo.controller;
+
+import com.oycbest.jdbcdemo.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author oyc
+ * @Description: 用户控制类
+ * @date 2018/7/8 22:10
+ */
+@Controller
+@RequestMapping("/jdbc")
+public class JdbcController {
+
+	@Resource
+	private JdbcTemplate jdbcTemplate;
+
+	@RequestMapping("/userlist")
+	public String getUserList(ModelMap map) {
+		String sql = "SELECT * FROM user";
+
+		List<User> userList = jdbcTemplate.query(sql, new RowMapper<User>() {
+			User user = null;
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setSex(rs.getString("sex"));
+				user.setAge(rs.getString("age"));
+				return user;
+			}
+		});
+		for (User user : userList) {
+			System.out.println(user.getName());
+		}
+		map.addAttribute("users", userList);
+		return "user";
+
+	}
+}
---
+++ src/main/java/com/oycbest/jdbcdemo/domain/User.java
@@ -0,0 +1,55 @@
+package com.oycbest.jdbcdemo.domain;
+
+
+/**
+ * @author oyc
+ * @Description:用户实体类
+ * @date 2018/7/8 22:51
+ */
+
+public class User{
+
+	//用户id
+	private String id;
+
+	//用户名称
+	private String name;
+
+	//户年龄
+	private String age;
+
+	//用户性别
+	private String sex;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+}

1e6bc9dd48b8d63d874fdac76ca0a4f88418aa8f
ouyangcheng <Oyc20Yqr1314>
2020-04-15 20:34:52 +0800

    用户controller修改

--- src/main/java/com/oycbest/jdbcdemo/controller/JdbcController.java
+++ src/main/java/com/oycbest/jdbcdemo/controller/JdbcController.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.sql.ResultSet;
@@ -21,30 +22,48 @@ import java.util.List;
 @RequestMapping("/jdbc")
 public class JdbcController {
 
-	@Resource
-	private JdbcTemplate jdbcTemplate;
+    @Resource
+    private JdbcTemplate jdbcTemplate;
 
-	@RequestMapping("/userlist")
-	public String getUserList(ModelMap map) {
-		String sql = "SELECT * FROM user";
+    @RequestMapping("/user")
+    @ResponseBody
+    public List<User> list(ModelMap map) {
+        String sql = "SELECT * FROM user";
+        List<User> userList = jdbcTemplate.query(sql, new RowMapper<User>() {
+            User user = null;
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setSex(rs.getString("sex"));
+                user.setAge(rs.getString("age"));
+                return user;
+            }
+        });
+        for (User user : userList) {
+            System.out.println(user.toString());
+        }
+        return userList;
 
-		List<User> userList = jdbcTemplate.query(sql, new RowMapper<User>() {
-			User user = null;
-			@Override
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setSex(rs.getString("sex"));
-				user.setAge(rs.getString("age"));
-				return user;
-			}
-		});
-		for (User user : userList) {
-			System.out.println(user.getName());
-		}
-		map.addAttribute("users", userList);
-		return "user";
+    }
 
-	}
+    @RequestMapping("/userList")
+    public String userList(ModelMap map) {
+        String sql = "SELECT * FROM user";
+        List<User> userList = jdbcTemplate.query(sql, new RowMapper<User>() {
+            User user = null;
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setSex(rs.getString("sex"));
+                user.setAge(rs.getString("age"));
+                return user;
+            }
+        });
+        map.addAttribute("users", userList);
+        return "user";
+    }
 }
